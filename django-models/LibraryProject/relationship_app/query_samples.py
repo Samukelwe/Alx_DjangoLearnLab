96439884e97db592f2c6725e015f372f81df073a
Samukelwe <samungalonde@gmail.com>
2025-02-23 16:30:23 +0200

    Changing and renaming a directory

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,5 @@
+from relationship_app.models import Book, Library, Author, Librarian
+
+"Author.objects.get(name=author_name)", "objects.filter(author=author)"
+"Library.objects.get(name=library_name)"
+"Librarian.objects.get(library=library_name )"
