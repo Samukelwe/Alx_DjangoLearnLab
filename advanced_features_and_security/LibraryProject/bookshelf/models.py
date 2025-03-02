b60ffae58df7a97859b9f5e1fbd536c75c12d12b
Samukelwe <samungalonde@gmail.com>
2025-03-02 09:06:14 +0200

    Advanced features and security

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,28 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
-
-# Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-from django.contrib.auth.models import BaseUserManager
-
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, profile_photo, password=None):
-        
+    def create_user(self, email, date_of_birth, profile_photo, password=None):        
         pass
 
     def create_superuser(self, email, date_of_birth, profile_photo, password=None):
-        
         pass
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
