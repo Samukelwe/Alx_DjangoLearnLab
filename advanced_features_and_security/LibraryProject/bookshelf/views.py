3ac35c246d71e7e4b5dbae01a7da85c3c09e3d01
Samukelwe <samungalonde@gmail.com>
2025-03-03 14:58:04 +0200

    Implementing security best practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ExampleForm
 
 <<<<<<< HEAD
 from django.contrib.auth.decorators import permission_required
