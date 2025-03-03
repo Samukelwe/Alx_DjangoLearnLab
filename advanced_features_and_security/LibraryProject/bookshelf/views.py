f937527752f42ec0c754ccec5fa224f3ecbf2f3e
Samukelwe <samungalonde@gmail.com>
2025-03-03 13:39:33 +0200

    Managing permissions and groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 <<<<<<< HEAD
 from django.contrib.auth.decorators import permission_required
 
@@ -7,5 +9,25 @@ def edit_view(request):
     
 =======
 
-# Create your views here.
+
 >>>>>>> 100fafa8c433f18a4f4d04a8da6a7d3056d4d76d
+
+
+def book_list(request):
+    
+    return HttpResponse("This is the book list view")
+
+def books(request):
+    
+    return HttpResponse("This is the books view")
+
+
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('app_name.can_view_books')
+def book_list(request):
+    # View logic
+
+@permission_required('app_name.can_edit_books')
+def books(request):
+    # View logic
