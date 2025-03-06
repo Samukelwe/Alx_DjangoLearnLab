0d570dae2b717480b7951daaea5b54c71c34bd98
Samukelwe <samungalonde@gmail.com>
2025-03-06 03:18:00 +0200

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 
 router = DefaultRouter()
@@ -10,5 +11,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  
 ]
+
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser  
+from rest_framework.authentication import TokenAuthentication
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,6 +12,17 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+ 
+
 class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows books to be viewed or edited.
+    Only authenticated users can access this endpoint.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access this endpoint
+
+
+
