2e7da13efce880a29a5ea13167995e14681ba6bb
Samukelwe <samungalonde@gmail.com>
2025-03-14 18:09:06 +0200

    Building custom views and generic views in Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,25 +1,14 @@
-from django.urls import path
-from .views import BookList
-from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
-from api.views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
-from rest_framework.authtoken.views import obtain_auth_token
-from django.urls import path, include
 
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
+
+from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-     path('books/', BookListView.as_view(), name='book-list'),
+    path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
-    path('', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  
 ]
-
-
 
 
