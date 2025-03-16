32cd5001966a05903c7948c6df7705eae30ef2e8
Samukelwe <samungalonde@gmail.com>
2025-03-16 02:06:40 +0200

    Implementing filtering, searching and ordering in Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,14 +1,19 @@
 
-
 from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import (
+    BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+)
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/<int:pk>/', BookCreateView.as_view(), name='book-create'),
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
-    path('books/delete<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    path("books/", BookListView.as_view(), name="books-list"),
+    path("book/<int:pk>/", BookDetailView.as_view(), name="book-list"),
+    path("book/", BookCreateView.as_view(), name="create-book"),
+    path("book-update/<int:pk>/", BookUpdateView.as_view(), name="book-update"),
+    path("delete/<int:pk>/", BookDeleteView.as_view(), name="book-delete"),
 ]
 
 
