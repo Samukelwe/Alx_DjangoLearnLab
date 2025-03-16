8d76ad229df9c29059ec9cfa2aa198524ec7bfbd
Samukelwe <samungalonde@gmail.com>
2025-03-16 16:02:40 +0200

    adding views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # Create your views here.
