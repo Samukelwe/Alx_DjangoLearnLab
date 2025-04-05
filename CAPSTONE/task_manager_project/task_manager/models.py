d6a7e09800c9d48e1e860d71a932f57fdf80aaf1
Samukelwe <samungalonde@gmail.com>
2025-04-05 07:59:47 +0200

    Added auto update feature to due date

--- CAPSTONE/task_manager_project/task_manager/models.py
+++ CAPSTONE/task_manager_project/task_manager/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    due_date = models.DateTimeField()
+    due_date = models.DateTimeField(auto_now_add=True)
     priority = models.CharField(max_length=10, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')])
     status = models.CharField(max_length=10, choices=[('incomplete', 'Incomplete'), ('complete', 'Complete')], default='incomplete')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
---
+++ CAPSTONE/task_manager_project/task_manager/readme.md
@@ -0,0 +1,9 @@
+This is my ALX BE capstone project, it is the culmination of what I have learned over the past couple of months.
+
+It is a task management API to help busy people manage their day to day tasks with ease.
+
+Using this API one is able, to add, view, edit and delete tasks as well as include their due date, priority and completion status.
+
+I have managed to do this through the support of my mentors and friends namely Brian Kaheru, Fortunate Alinda and Adamou Nikiema.
+
+Email: samungalonde@gmail.com
